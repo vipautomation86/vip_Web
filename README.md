c9fae9a7809846e39ef603bbec52cfbff2f49a2e
vipautomation86 <vipautomation86@gmail.com>
2023-05-16 05:01:42 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#TypeScript MERN AMAZON
+
+## Lessons
+
+1. Introdection
+2. Install Tools
+3. Create TypeScript React App By Vite
+4. Create Git Repository
---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+
+    <div>
+      <header>TS Amazon</header>
+      <main></main>
+      <footer>All right reserved</footer>
+
+    </div>
+
+
+  )
+}
+
+export default App
